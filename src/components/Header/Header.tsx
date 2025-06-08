8e53ec76f80871d2fc8a120c752ad864ff47bba0
shamseer8877 <alishamseer@gmail.com>
2025-06-08 12:37:54 +0530

    remove unwanted lines

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HeaderTop from "./HeaderTop";
 import HeaderBottom from "./HeaderBottom";
 
--- src/components/Header/HeaderBottom.tsx
+++ src/components/Header/HeaderBottom.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const HeaderBottom = () => {
   return <div>HeaderBottom</div>;
 };
